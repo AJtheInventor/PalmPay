33726583f33aaba072db7b5e1ab8e7ebecc0f08e
A.J. Shulman <shulman.aj@gmail.com>
2018-05-30 17:39:40 -0400

    Third commit

--- php.php
+++ php.php
@@ -1,7 +1,10 @@
-<?php 
-
-  $email_from = 'shulman.aj@gmail.com';
-  $email_subject = "New Form Submission";
-  $email_body = "You have received a new message from the user $name.\n $email.\n".;
-  mail($email_from,$email_subject,$email_body,$headers);
+<?php
+$to = $myemail;
+$email_subject = "Contact form submission: $name";
+$email_body = "You have received a new user. ".
+" Here are the details:\n Name: $name \n ".
+$headers = "From: $to\n";
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+header('Location: contact-form-thank-you.html');
 ?>
